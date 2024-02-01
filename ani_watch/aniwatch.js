6453a46bfcb0f5796f57ceccbf05c1e3a985a0dd
Rushi222003 <146449005+Rushi222003@users.noreply.github.com>
2024-02-01 18:34:42 +0530

    Add files via upload

---
+++ ani_watch/aniwatch.js
@@ -0,0 +1,56 @@
+function toggleDropdown()
+{
+    var dropdown = document.getElementById("dropdown-content");
+
+    if (dropdown.style.display === "none" || dropdown.style.display === "") 
+    {
+        dropdown.style.display = "block";
+    }           
+    else
+    {
+        dropdown.style.display = "none";
+    }   
+}
+
+function toggleDropdown1()
+{
+    var dropdown = document.getElementById("dropdown-content1");
+
+    if (dropdown.style.display === "none" || dropdown.style.display === "") 
+    {
+        dropdown.style.display = "block";
+    }           
+    else
+    {
+        dropdown.style.display = "none";
+    }   
+}
+
+function toggleDropdown2()
+{
+    var dropdown = document.getElementById("dropdown-content2");
+
+    if (dropdown.style.display === "none" || dropdown.style.display === "") 
+    {
+        dropdown.style.display = "block";
+    }           
+    else
+    {
+        dropdown.style.display = "none";
+    }   
+}
+
+function toggleDropdown3()
+{
+    var dropdown = document.getElementById("dropdown-content3");
+
+    if (dropdown.style.display === "none" || dropdown.style.display === "") 
+    {
+        dropdown.style.display = "block";
+    }           
+    else
+    {
+        dropdown.style.display = "none";
+    }   
+}
+
